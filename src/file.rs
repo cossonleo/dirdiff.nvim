603ac1c513b39c53a32d6ecf379c5688545c7999
cossonleo <cossonleo@foxmail.com>
2019-08-30 06:48:16 +0800

    upd

--- src/file.rs
+++ src/file.rs
@@ -1,5 +1,9 @@
 use async_std::fs;
+use async_std::prelude::*;
+use futures::future::FutureExt;
+use std::future::Future;
 
+use failure::{format_err, Fallible};
 use magic::{flags::*, Cookie};
 
 use std::collections::BTreeSet;
@@ -29,6 +33,30 @@ macro_rules! contain_extension {
     }};
 }
 
+macro_rules! path_push {
+    ($parent: expr, $sub: expr) => {{
+        let mut parent = $parent.clone();
+        parent.push($sub.as_ref());
+        parent
+    }};
+}
+
+macro_rules! get_fut_fd {
+    ($p: expr) => {{
+        let md = match async_std::fs::metadata(&$p).await {
+            Err(_) => return false,
+            Ok(md) => md,
+        };
+
+        let mut buf = vec![0u8; md.len() as usize];
+        let fd = match fs::File::open(&$p).await {
+            Err(_) => return false,
+            Ok(fd) => fd,
+        };
+        (fd, buf)
+    }};
+}
+
 //trait FileFiltor<P: AsRef<Path>> {
 //    fn is_select(&self, p: P) -> bool;
 //}
@@ -36,10 +64,25 @@ macro_rules! contain_extension {
 trait FileFilter {
     fn is_select(&self, p: &Path) -> bool;
 }
+enum DiffStatus {
+    Add(PathBuf),
+    Delete(PathBuf),
+    Changed(PathBuf),
+}
 
 struct DefaultFiltor {
     magic_cookie: Option<Cookie>,
 }
+
+struct BlackExtFiltor(Vec<OsString>);
+struct WhiteExtFiltor(Vec<OsString>);
+
+pub struct DirComare {
+    opt: Opt,
+    mcookie: Option<Cookie>,
+    filter: Vec<Box<dyn FileFilter>>,
+}
+pub struct CompareBuilder(Opt);
 
 impl DefaultFiltor {
     fn new() -> Self {
@@ -72,15 +115,11 @@ impl FileFilter for DefaultFiltor {
     }
 }
 
-struct BlackExtFiltor(Vec<OsString>);
-
 impl FileFilter for BlackExtFiltor {
     fn is_select(&self, p: &Path) -> bool {
         !contain_extension!(self.0, p)
     }
 }
-
-struct WhiteExtFiltor(Vec<OsString>);
 
 impl FileFilter for WhiteExtFiltor {
     fn is_select(&self, p: &Path) -> bool {
@@ -88,14 +127,10 @@ impl FileFilter for WhiteExtFiltor {
     }
 }
 
-pub struct DirComare {
-    opt: Opt,
-    mcookie: Option<Cookie>,
-    filter: Vec<Box<dyn FileFilter>>,
-}
-
 // 公有
 impl DirComare {
+    fn run() {}
+
     pub fn is_select(&self, p: impl AsRef<Path>) -> bool {
         let p = p.as_ref();
         for f in self.filter.iter() {
@@ -105,9 +140,70 @@ impl DirComare {
         }
         true
     }
-}
 
-pub struct CompareBuilder(Opt);
+    async fn is_diff(&self, sub_path: impl AsRef<Path>) -> bool {
+        let p1 = path_push!(self.opt.base, sub_path);
+        let p2 = path_push!(self.opt.branch, sub_path);
+
+        let (fd1, buf1) = get_fut_fd!(p1);
+        let (fd2, buf2) = get_fut_fd!(p2);
+
+        futures::join!(fd1, fd2);
+
+        false
+    }
+
+    async fn visit_dir(parent: PathBuf, f: &mut impl FnMut(PathBuf)) {
+        let fut = async move {
+            let mut dir = fs::read_dir(parent).await.unwrap();
+            for entry in dir.next().await {
+                if let Ok(entry) = entry {
+                    let path = entry.path();
+                    if path.is_dir() {
+                        Self::visit_dir(path, f).await;
+                    } else {
+                        f(path);
+                    }
+                }
+            }
+        };
+        fut.await;
+    }
+
+    //async fn visit_dir(parent: PathBuf) {
+    //        let mut dir = fs::read_dir(parent).await.unwrap();
+    //        for entry in dir.next().await {
+    //            if let Ok(entry) = entry {
+    //                let path = entry.path();
+    //                if path.is_dir() {
+    //                    Box::new(Self::visit_dir(path).await);
+    //                } else {
+    //                    //f(path);
+    //                }
+    //            }
+    //        }
+    //}
+
+    //    async fn visit_dir<F>(parent: impl AsRef<Path>, f: &mut F) -> Fallible<()>
+    //    where
+    //        F: FnMut(PathBuf),
+    //    {
+    //        let parent = parent.as_ref();
+    //        let mut dir = fs::read_dir(parent).await?;
+    //        for entry in dir.next().await {
+    //            if let Ok(entry) = entry {
+    //                let path = entry.path();
+    //                if path.is_dir() {
+    //    //                Box::new(Self::visit_dir(&path, f)).await?;
+    //                    Box::new(Self::visit_dir(&path, f).await?);
+    //                } else {
+    //                    f(path);
+    //                }
+    //            }
+    //        }
+    //        Ok(())
+    //    }
+}
 
 impl CompareBuilder {
     fn with(opt: Opt) -> Self {
@@ -116,7 +212,7 @@ impl CompareBuilder {
 
     fn build_dir_compare(self) -> DirComare {
         let filter = Self::get_filters(&self.0);
-        DirComare{
+        DirComare {
             opt: self.0,
             filter,
             mcookie: Self::get_magic_cookie(),
